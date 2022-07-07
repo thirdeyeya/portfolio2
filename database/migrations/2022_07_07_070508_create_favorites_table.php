3bd9656dad4f0470757d76891bb3f72bda223c62
Sakika TsuTsui <longlongjourney@outlook.jp>
2022-07-07 07:42:56 +0000

    commit

---
+++ database/migrations/2022_07_07_070508_create_favorites_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('favorite_id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('music_id')->unsigned();
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('music_id')->references('id')->on('musics')->onDelete('cascade');
+                $table->unique(['user_id', 'music_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
--- resources/views/admin/music/index.blade.php
+++ resources/views/admin/music/index.blade.php
@@ -5,67 +5,51 @@
 <div class="container">
   <div class="row">
     <div class="col-3">
-  <form action="{{ action('Admin\MusicController@index') }}" method="get"> 
-    <div>
-    <h4>ジャンル</h4>
-      <select name ="genre_id">
-        <option value="0" @if($cond_genre == 0) selected="selected" @endif>---</option>
-        @foreach( $genres as $genre)
-          <option value="{{ $genre->id }}" @if($cond_genre == $genre->id) selected="selected" @endif>{{ $genre->title }}</option>
-        @endforeach  
-      </select>
-      <input type="submit" class="btn btn-primary" value="検索"> 
+      <form action="{{ action('Admin\MusicController@index') }}" method="get"> 
+        <div>
+          <h4>ジャンル</h4>
+          <select name ="genre_id">
+            <option value="0" @if($cond_genre == 0) selected="selected" @endif>---</option>
+            @foreach( $genres as $genre)
+              <option value="{{ $genre->id }}" @if($cond_genre == $genre->id) selected="selected" @endif>{{ $genre->title }}</option>
+            @endforeach  
+          </select>
+          <input type="submit" class="btn btn-primary" value="検索"> 
+        </div>
+        <br>
+        <div>
+          <h4>曲調</h4>
+          <select name ="music_tone_id">
+            <option value="0" @if($cond_music_tone == 0) selected="selected" @endif>---</option>
+            @foreach( $music_tones as $music_tone)
+              <option value="{{ $music_tone->id }}" @if($cond_music_tone == $music_tone->id) selected="selected" @endif>{{ $music_tone->title }}</option>
+            @endforeach  
+          </select>
+        </div>  
+        <br>
+        <div>
+          <h4>性別</h4>
+            <select name ="gender_id">
+              <option value="0" @if($cond_gender == 0) selected="selected" @endif>---</option>
+              @foreach($genders as $str => $num)
+                <option value="{{ $num }}" @if($cond_gender == $num) selected="selected" @endif>{{ $str }}</option>
+              @endforeach 
+            </select>
+        </div>    
+      </form> 
     </div>
-      <br>
-    <div>
-    <h4>曲調</h4>
-      <select name ="music_tone_id">
-        <option value="0" @if($cond_music_tone == 0) selected="selected" @endif>---</option>
-        @foreach( $music_tones as $music_tone)
-          <option value="{{ $music_tone->id }}" @if($cond_music_tone == $music_tone->id) selected="selected" @endif>{{ $music_tone->title }}</option>
-        @endforeach  
-      </select>
-    </div>  
-      <br>
-    <div>
-    <h4>性別</h4>
-      <select name ="gender_id">
-        <option value="0" @if($cond_gender == 0) selected="selected" @endif>---</option>
-        @foreach($genders as $str => $num)
-          <option value="{{ $num }}" @if($cond_gender == $num) selected="selected" @endif>{{ $str }}</option>
-        @endforeach 
-      </select>
-    </div>    
-  </form> 
-  </div>
-  <div class="container">
-  <div class="row">
-    <div class="col">
-      <table class="table table-white">
-        $start = 0;
-        $end = 4;
-        for($search = $start; $search < $end; $search++){
-          if($search == $end){
-            $search = $start
-          }
-        }  
-            @foreach($posts as $search)
-            <tr>
-              <td>
-              <img src="{{ \Str::limit($search->image_path, 250) }}" alt="cdジャケット" width="180" height="180" /> 
-              <p>曲名：{{ \Str::limit($search->title, 250) }}</p>
-        　　　　<p>アーティスト名：{{ \Str::limit($search->artist_name, 250) }}</p>
-        　　 　  <a href="{{ \Str::limit($search->anime_official_cite, 250) }}">アニメ公式</a>
-        　　  　<br>  
-        　　　　  <a href="{{ \Str::limit($search->artist_official_cite, 250) }}">アーティスト公式</a>
-        　 　　<br>
-        　   　<a href="{{ action('Admin\MusicController@add') }}">コメント</a>　
-        　　　　</td>
-            </tr>
-            @endforeach
-        </table>
-      </div>
+      @foreach($posts as $search)
+        <div class="col-3">
+          <img src="{{ \Str::limit($search->image_path, 250) }}" alt="cdジャケット" width="180" height="180" /> 
+          <p>曲名：{{ \Str::limit($search->title, 250) }}</p>
+        　<p>アーティスト名：{{ \Str::limit($search->artist_name, 250) }}</p>
+        　<a href="{{ \Str::limit($search->anime_official_cite, 250) }}">アニメ公式</a>
+        　<br>  
+        　<a href="{{ \Str::limit($search->artist_official_cite, 250) }}">アーティスト公式</a>
+        　<br>
+        　<a href="{{ action('Admin\MusicController@add') }}">コメント</a>
+        </div>    　
+      @endforeach
     </div>
-  </div>
 </div>
 @endsection
